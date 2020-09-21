104b265d71c3e141f7b0613945453258ab8ab73d
Chris <christopher.lopez.09@gmail.com>
2020-09-21 14:57:22 -0400

    finish last phase

--- TicTacToe/lib/super_computer_player.rb
+++ TicTacToe/lib/super_computer_player.rb
@@ -2,6 +2,14 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    non_losers = []
+    node.children.each do |child|
+      return child.prev_move_pos if child.winning_node?(mark)
+      non_losers << child unless child.losing_node?(mark)
+    end
+    raise "There are no non losing nodes" if non_losers.empty?
+    non_losers.sample.prev_move_pos
   end
 end
 
